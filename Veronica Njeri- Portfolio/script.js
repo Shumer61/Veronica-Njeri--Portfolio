36585985c66ed26fcd4f4d5312a3dd2fbf777698
shuma <39579339+Shumer61@users.noreply.github.com>
2025-06-06 10:02:56 +0300

    Add files via upload

---
+++ Veronica Njeri- Portfolio/script.js
@@ -0,0 +1,14 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 3000); // 3 seconds per slide
+ 
